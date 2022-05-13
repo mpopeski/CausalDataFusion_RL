92ecca239970c01cab790abd66d1886de0448b2b
Martin Popeski <mpopeski@ethz.ch>
2022-05-13 17:52:19 +0200

    new config - more reward states

--- test_Vmax.py
+++ test_Vmax.py
@@ -9,7 +9,7 @@ import os
 def main(K_obs, K_int, m, path):
     path_ = path + f"Vmax/{K_obs}_{K_int}_{m}/"
     os.makedirs(path_, exist_ok=True)
-    env = TabularMDP(2, 5, 2, [-1,0,1], 8)
+    env = TabularMDP(2, 5, 2, [-1,0,1], 8, n_reward_states = 12)
     env.observational_data(K_obs)
     integration = ["ignore", "naive", "controlled"]
     gamma = 0.9
@@ -30,8 +30,7 @@ def main(K_obs, K_int, m, path):
         results.to_csv(path_ + f"results{rep}.csv")
 
 if __name__ == "__main__":
-    configs = [(10000, 10000, 1000), (12500, 7500, 1000), (15000, 5000, 1000),
-               (7500, 12500, 1000), (5000, 15000, 1000)]
-    path = "../new_experiments/"
+    configs = [(10000, 10000, 1000), (15000, 7500, 1000), (20000, 7500, 1000)]
+    path = "../new_experiments12/"
     for config in configs:
         main(config[0], config[1], config[2], path)
