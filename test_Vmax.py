82d362e82681cc4a370304ee4c7ed53e655a1cbf
Martin Popeski <mpopeski@ethz.ch>
2022-05-19 00:39:11 +0200

    dense-long episode

--- test_Vmax.py
+++ test_Vmax.py
@@ -9,7 +9,7 @@ import os
 def main(K_obs, K_int, m, path):
     path_ = path + f"Vmax/{K_obs}_{K_int}_{m}/"
     os.makedirs(path_, exist_ok=True)
-    env = TabularMDP(2, 5, 2, [-1,0,1], 8, n_reward_states = 20)
+    env = TabularMDP(2, 5, 2, [-1,0,1], 80, n_reward_states = 20)
     env.observational_data(K_obs)
     integration = ["ignore", "naive", "controlled"]
     gamma = 0.9
@@ -30,7 +30,7 @@ def main(K_obs, K_int, m, path):
         results.to_csv(path_ + f"results{rep}.csv")
 
 if __name__ == "__main__":
-    configs = [(10000, 20000, 1000), (30000, 20000, 1000),(50000, 20000, 1000)]
-    path = "../experiments_v10_dense/"
+    configs = [(1000, 2000, 1000), (3000, 2000, 1000),(5000, 2000, 1000)]
+    path = "../experiments_v10_longE/"
     for config in configs:
         main(config[0], config[1], config[2], path)
