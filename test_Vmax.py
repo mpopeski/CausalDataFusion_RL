9ec5ee5ef72d4e1e4627ac0f45ca40faca63fdb6
Martin Popeski <mpopeski@ethz.ch>
2022-05-20 23:40:13 +0200

    short episode

--- test_Vmax.py
+++ test_Vmax.py
@@ -14,7 +14,7 @@ def main(config):
     path = config[3]
     path_ = path + f"Vmax/{K_obs}_{K_int}_{m}/"
     os.makedirs(path_, exist_ok=True)
-    env = TabularMDP(2, 5, 2, [-1,0,1], 80, n_reward_states = 12)
+    env = TabularMDP(2, 5, 2, [-1,0,1], 10, n_reward_states = 12)
     
     path_environment = path_ + "environment/"
     env.save_env(path_environment)
@@ -39,11 +39,11 @@ def main(config):
         results.to_csv(path_ + f"results{rep}.csv")
 
 if __name__ == "__main__":
-    path = "../experiments_v16/"
-    configs = [(2500, 2000, 1000, path), (2900, 2000, 1000, path), (3300, 2000, 1000, path),
-               (2600, 2000, 1000, path), (3000, 2000, 1000, path), (3400, 2000, 1000, path),
-               (2700, 2000, 1000, path), (3100, 2000, 1000, path), (3500, 2000, 1000, path),
-               (2800, 2000, 1000, path), (3200, 2000, 1000, path), (3600, 2000, 1000, path)]
+    path = "../experiments_v17/"
+    configs = [(25000, 20000, 1000, path), (29000, 20000, 1000, path), (33000, 20000, 1000, path),
+               (26000, 20000, 1000, path), (30000, 20000, 1000, path), (34000, 20000, 1000, path),
+               (27000, 20000, 1000, path), (31000, 20000, 1000, path), (35000, 20000, 1000, path),
+               (28000, 20000, 1000, path), (32000, 20000, 1000, path), (36000, 20000, 1000, path)]
     with Pool(processes=None) as p:
         p.map(main, configs)
 
