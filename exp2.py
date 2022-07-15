1e5e6ff1c76420da679a41064e2605d77a356381
Martin Popeski <mpopeski@ethz.ch>
2022-07-15 15:48:22 +0200

    exp2 new setup

--- exp2.py
+++ exp2.py
@@ -4,23 +4,28 @@
 import pandas as pd
 import os
 from multiprocessing import Pool
+import copy
 
 from Rmax import R_MAX
 from Vmax import Vmax
 from MDP_environments import TabularMDP
 
+# the environment
+base_env = TabularMDP(state_values = 5, action_values = 2, H = 500, default_prob = 3, n_reward_states=12, policy = "random", 
+                     simpson = True, conf_values = 4)
 
-def main(config):
-    path = config[0]
-    conf_val = config[1]
+path = "../final_exp2/"
+K_obs = 2500 
+
+def main(conf_val):
+
+    env = copy.deepcopy(base_env)
+    env.get_confounder_dist(conf_val)
+    
+    print("collecting observational data")
+    data = env.get_obs_data(K_obs)   
     m = 1000
     K_int = 500
-    K_obs = 5000
-    # the environment
-    env = TabularMDP(state_values = 5, action_values = 2, H = 800, n_reward_states=12, policy = "random", 
-                     simpson = True, conf_values = conf_val)
-    print("collecting observational data")
-    env.observational_data(K_obs)   
     
     integration = ["ignore", "controlled", "controlled_FD"]
     integration_index = ["ignore_Rmax", "ignore_Vmax", "controlled_Rmax",\
@@ -31,31 +36,31 @@ def main(config):
     Rmax = 1
     reps = 5
     
-    path_ = path + f"{conf_val}_{K_int}/" 
+    path_ = path + f"{conf_val}/" 
     os.makedirs(path_, exist_ok=True)
-        
+    
     for rep in range(reps):
         results = []
         for integ in integration:
             model1 = R_MAX(env, gamma, m, eta, Rmax, K_int)
-            model1.initialize(integ)
+            model1.initialize(data, integ)
             model1.learn()
             results.append(model1.reward)
             
             model2 = Vmax(env, gamma, m, eta, Rmax, K_int)
-            model2.initialize(integ)
+            model2.initialize(data, integ)
             model2.learn()
             results.append(model2.reward)
             
         results = pd.DataFrame(results, index = integration_index).T
         results.to_csv(path_ + f"results{rep}.csv")
-    
-    env.data["r"].to_csv(path_ + "obs_rew.csv")
+          
+    data["r"].to_csv(path_ + "obs_rew.csv")
 
 if __name__ == "__main__":
-    path = "../final_exp2_repeat/"
-    
-    configs = [(path, 4), (path, 8), (path, 16)]
+
+    conf_vals = [4,8,16,32]
 
     with Pool(processes=None) as p:
-        p.map(main, configs)
+        p.map(main, conf_vals)
+        
